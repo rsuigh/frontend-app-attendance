8f2833810aa982d8c5c2643e316977c263fde85f
Rodrigo <suigh.rodrigo@gmail.com>
2024-06-05 09:49:51 -0300

    fazendo fetch do edx

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,10 +1,14 @@
 import { Container } from '@openedx/paragon';
+import { FetchCoursesList } from "./data/TestRequest"
+
 
 const ExamplePage = () => (
   <main>
     <Container className="py-5">
       <h1>Example Page</h1>
       <p>Hello world!</p>
+      teste
+      <FetchCoursesList/>
     </Container>
   </main>
 );
