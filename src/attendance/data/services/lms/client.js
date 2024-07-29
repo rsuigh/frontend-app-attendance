a5bbb9305ba41a6e62f174fbd151041eaecd292e
Rodrigo Suigh <suigh.rodrigo@gmail.com>
2024-07-29 12:43:35 -0300

    refactored envs

--- src/attendance/data/services/lms/client.js
+++ src/attendance/data/services/lms/client.js
@@ -1,12 +1,11 @@
 import { getConfig } from "@edx/frontend-platform";
 
 // const ATTENDANCE_TOKEN = 'Gfdn0E2RT4RNdzSYdW5mw6Lwqsey7f'
-const ATTENDANCE_TOKEN = getConfig().ATTENDANCE_TOKEN
 const client = (method, data=null, url) => {
-    console.log(ATTENDANCE_TOKEN)
+    console.log(getConfig().ATTENDANCE_TOKEN)
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
-    myHeaders.append("Authorization", `Bearer ${ATTENDANCE_TOKEN}`)
+    myHeaders.append("Authorization", `Bearer ${getConfig().ATTENDANCE_TOKEN}`)
     const requestOptions = {
         method: method,
         headers: myHeaders,
