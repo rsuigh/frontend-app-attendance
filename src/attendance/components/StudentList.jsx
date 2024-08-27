a9f0926f45bdd09cfd83f4f367a09a56cb3554fd
Rodrigo Suigh <suigh.rodrigo@gmail.com>
2024-08-27 18:56:20 -0300

    test out

--- src/attendance/components/StudentList.jsx
+++ src/attendance/components/StudentList.jsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react';
 
 import * as utils from '../data/services/lms/utils';
-import { getEnrroledStudentListUrl, 
-    postAttendanceUrl, 
-    getEnrollmentStudentDateJoinedUrl 
+import {
+    getEnrroledStudentListUrl,
+    postAttendanceUrl,
+    getEnrollmentStudentDateJoinedUrl
 } from '../data/services/lms/urls';
-import { Button, Col, Form, Alert} from '@openedx/paragon';
+import { Button, Col, Form, Alert } from '@openedx/paragon';
 import { getAuthenticatedUser } from '@edx/frontend-platform/auth';
 import client from '../data/services/lms/client';
 
@@ -14,21 +15,21 @@ import client from '../data/services/lms/client';
 
 
 
-const StudentList = ({courseId}) => {
+const StudentList = ({ courseId }) => {
     const [list, setList] = useState([]);
     //const [list, setList] = useState(hcjson.results);
     const [loading, setLoading] = useState(true);
     const [showAlert, setShowAlert] = useState(false)
     const [showErrorAlert, setShowErrorAlert] = useState(false)
     const [studentsPresent, setStudentsPresent] = useState([])
-    
-    const { get, post, stringifyUrl} = utils;
-    
+
+    const { get, post, stringifyUrl } = utils;
+
     const today = new Date();
     const date = today.setDate(today.getDate())
     const todayDate = new Date(date).toISOString().split("T")[0]
 
-    
+
     // for development use this:
     // const [studentsPresent, setStudentsPresent] = useState(
     //     [
@@ -45,11 +46,11 @@ const StudentList = ({courseId}) => {
     //             "present": false
     //         }
     //     ])
-    
+
     const handleStudentsPresent = (event, username) => {
         const newStudentsPresentList = studentsPresent?.map(student => {
             if (student.username == username) {
-                return {username, present: event.target.checked}
+                return { username, present: event.target.checked }
             }
             return student
         })
@@ -79,7 +80,7 @@ const StudentList = ({courseId}) => {
         client('POST', data, postAttendanceUrl())
             .then((response) => {
                 debugger
-                if (!response.ok){
+                if (!response.ok) {
                     setShowErrorAlert(true)
                     setShowAlert(false)
                 } else {
@@ -91,94 +92,92 @@ const StudentList = ({courseId}) => {
                 setShowErrorAlert(true)
                 setShowAlert(false)
             })
-            .finally(() =>{
+            .finally(() => {
                 setLoading(false)
             })
     }
-  
+
     useEffect(() => {
-      // Define the async function
+        // Define the async function
         const fetchData = async () => {
-          try {
-              const response = await get(getEnrroledStudentListUrl(courseId.courseId));
-              const response2 = await get(getEnrollmentStudentDateJoinedUrl(courseId.courseId, "suigh"));
-              console.log("DATA REPONSE2: "+response2.data);
-              setList(response.data.results);
-              setLoading(false)
-              return response.data.results
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          } finally {
-            setLoading(false)
-          }
+            try {
+                const response = await get(getEnrroledStudentListUrl(courseId.courseId));
+                setList(response.data.results);
+                setLoading(false)
+                return response.data.results
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            } finally {
+                setLoading(false)
+            }
         }
-  
+
         fetchData().then((students) => {
             setStudentsPresent(students?.map(student => {
-                return {username: student.username, present: false}
+                return { username: student.username, present: false }
             }))
-       }); 
+        });
     }, [courseId]); // The empty dependency array means this effect runs once when the component mounts
 
     return (
-      <div>
-        {showAlert && <Alert 
-                        variant="success" 
-                        actions={[
-                            <Button onClick={() => setShowAlert(false)}>Fechar</Button>,
-                        ]} >
-            <Alert.Heading>Presença enviada</Alert.Heading>
-        </Alert>}
-        {showErrorAlert && <Alert 
-                        variant="danger" 
-                        actions={[
-                            <Button onClick={() => setShowErrorAlert(false)}>Fechar</Button>,
-                        ]} >
-            <Alert.Heading>Erro. Entre em contato com o administrador do sistema</Alert.Heading>
-        </Alert>}
+        <div>
+            {showAlert && <Alert
+                variant="success"
+                actions={[
+                    <Button onClick={() => setShowAlert(false)}>Fechar</Button>,
+                ]} >
+                <Alert.Heading>Presença enviada</Alert.Heading>
+            </Alert>}
+            {showErrorAlert && <Alert
+                variant="danger"
+                actions={[
+                    <Button onClick={() => setShowErrorAlert(false)}>Fechar</Button>,
+                ]} >
+                <Alert.Heading>Erro. Entre em contato com o administrador do sistema</Alert.Heading>
+            </Alert>}
 
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
-            <Form onSubmit={onSubmit}>
-                <Form.Row>
-                    <Form.Group as={Col}>
-                        <Form.Control 
-                            name='date' 
-                            type="date" 
-                            floatingLabel="Data" 
-                            className='mt-4'
-                            defaultValue={todayDate}
-                        />
-                    </Form.Group>
-                    <Form.Group as={Col}>
-                        <Form.Control name='class_type' as="select" floatingLabel="Tipo de aula" className='mt-4' defaultValue={{ label: "Aula normal", value: "an" }}> 
-                            <option value="an">Aula normal</option>
-                            <option value="ar">Reposição</option>
-                        </Form.Control>
-                    </Form.Group>
-                </Form.Row>
-                <p>Lista de estudantes</p>
-                <Button onClick={() => markAllCheckBoxes()}>Marcar todos</Button>
-                <ul className='mt-4'>                    
-                    {list.map(item => (
-                        <li key={item.user_id}> 
-                            <Form.Checkbox 
-                                className="flex-column flex-sm-row" 
-                                onChange={(event) => handleStudentsPresent(event, item.username)}
-                                checked={studentsPresent.find(student => student.username === item.username)?.present}
+            {loading ? (
+                <p>Loading...</p>
+            ) : (
+                <Form onSubmit={onSubmit}>
+                    <Form.Row>
+                        <Form.Group as={Col}>
+                            <Form.Control
+                                name='date'
+                                type="date"
+                                floatingLabel="Data"
+                                className='mt-4'
+                                defaultValue={todayDate}
+                            />
+                        </Form.Group>
+                        <Form.Group as={Col}>
+                            <Form.Control name='class_type' as="select" floatingLabel="Tipo de aula" className='mt-4' defaultValue={{ label: "Aula normal", value: "an" }}>
+                                <option value="an">Aula normal</option>
+                                <option value="ar">Reposição</option>
+                            </Form.Control>
+                        </Form.Group>
+                    </Form.Row>
+                    <p>Lista de estudantes</p>
+                    <Button onClick={() => markAllCheckBoxes()}>Marcar todos</Button>
+                    <ul className='mt-4'>
+                        {list.map(item => (
+                            <li key={item.user_id}>
+                                <Form.Checkbox
+                                    className="flex-column flex-sm-row"
+                                    onChange={(event) => handleStudentsPresent(event, item.username)}
+                                    checked={studentsPresent.find(student => student.username === item.username)?.present}
                                 >
-                                {item.username}
-                            </Form.Checkbox>
-                        </li>
-                    ))}
-                </ul>
-                <Button type='submit'>Enviar</Button>
-            </Form>
-        )}
-        
-      </div>
+                                    {item.username}
+                                </Form.Checkbox>
+                            </li>
+                        ))}
+                    </ul>
+                    <Button type='submit'>Enviar</Button>
+                </Form>
+            )}
+
+        </div>
     );
-  };
-  
-  export default StudentList;
+};
+
+export default StudentList;
